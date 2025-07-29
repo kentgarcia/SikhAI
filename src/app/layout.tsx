54dd2dd6163ec1c7d9a85d50519860ebbf1c119e
Kent Garcia <kentgarcia.dgl@gmail.com>
2025-07-29 19:29:31 +0000

    Use poppins as the default font. corner radius for input is 10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
 
 export const metadata: Metadata = {
   title: 'New App',
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-muted/40">
+      <body className={`${poppins.className} bg-muted/40`}>
         <main className="mx-auto max-w-sm h-screen bg-background shadow-lg overflow-y-auto">
           {children}
         </main>
